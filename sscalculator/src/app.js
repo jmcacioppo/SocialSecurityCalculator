beec64389a42336bb014d2212d47c28e8e0523d1
jmcacioppo <jcacioppo@PIE_MAIN.pietech.com>
2017-06-12 16:50:47 -0400

    Routing works

--- sscalculator/src/app.js
+++ sscalculator/src/app.js
@@ -13,12 +13,14 @@ export class App {
         name: 'personalinfo', title: 'Personal Info', nav: true},
       { route: 'retirementinfo', moduleId: 'aboutyou/retirementinfo',
         name: 'retirementinfo', title: 'Retirement Info', nav: true},
+
       { route: 'benefits', moduleId: 'benefits/benefits',
         name: 'benefits', title: 'Benefits', nav: true},
       { route: 'costofliving', moduleId: 'benefits/costOfLiving',
         name: 'costofliving', title: 'Cost of Living', nav: true},
-      { route: 'other benefits', moduleId: 'benefits/other',
+      { route: 'other', moduleId: 'benefits/other',
         name: 'otherbenefits', title: 'Other Benefits', nav: true},
+        
       { route: 'results', moduleId: 'results/results',
         name: 'results', title: 'Results', nav: true}
     ]);
