3239df28f7a26bebd05fe4d251cd66bb0e6aa07e
Sean McHugh <smchugh@PIE_MAIN.pietech.com>
2017-06-12 15:33:57 -0400

    first router stuff

--- sscalculator/src/app.js
+++ sscalculator/src/app.js
@@ -4,4 +4,24 @@ export class App {
   constructor() {
     this.message = 'Social Security Calculator!';
   }
+
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = "Social Security Calculator";
+    config.map([
+      { route: ['', 'personalinfo'], moduleId: 'personalinfo',
+        name: 'personalinfo', title: 'Personal Info', nav: true},
+      { route: 'retirementinfo', moduleId: 'retirementinfo',
+        name: 'retirementinfo', title: 'Retirement Info', nav: true},
+      { route: 'benefits', moduleId: 'benefits',
+        name: 'benefits', title: 'Benefits', nav: true},
+      { route: 'costofliving', moduleId: 'costofliving',
+        name: 'costofliving', title: 'Cost of Living', nav: true},
+      { route: 'other benefits', moduleId: 'other benefits',
+        name: 'otherbenefits', title: 'Other Benefits', nav: true},
+      { route: 'results', moduleId: 'results',
+        name: 'results', title: 'Results', nav: true}
+    ]);
+  }
+
 }
