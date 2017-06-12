a3ea2e916d107afc13d63d58e0f4414486c9bc06
jmcacioppo <jcacioppo@pietech.com>
2017-06-12 14:54:37 -0400

    personalinfo files added

--- sscalculator/scripts/app-bundle.js
+++ sscalculator/scripts/app-bundle.js
@@ -61,6 +61,25 @@ define('main',['exports', './environment'], function (exports, _environment) {
     });
   }
 });
+define('personalinfo',["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var personalinfo = exports.personalinfo = function personalinfo() {
+        _classCallCheck(this, personalinfo);
+
+        this.message = "Personal Info";
+    };
+});
 define('resources/index',["exports"], function (exports) {
   "use strict";
 
@@ -71,4 +90,5 @@ define('resources/index',["exports"], function (exports) {
   function configure(config) {}
 });
 define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=\"bootstrap/css/bootstrap.css\"></require><nav class=\"navbar navbar-default\"><div class=\"container-fluid\"><div class=\"navbar-header\"><button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\"><span class=\"sr-only\">Toggle navigation</span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span></button> <a class=\"navbar-brand\" href=\"#\">Social Security Calculator</a></div><div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\"><ul class=\"nav navbar-nav\"><li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">About You<span class=\"caret\"></span></a><ul class=\"dropdown-menu\"><li class=\"active\"><a href=\"#\">Personal Info</a></li><li><a href=\"#\">Retirement Info</a></li></ul></li><li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Benefits<span class=\"caret\"></span></a><ul class=\"dropdown-menu\"><li><a href=\"#\">Benefits</a></li><li><a href=\"#\">Cost of Living</a></li><li><a href=\"#\">Other Benefits</a></li></ul></li><li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Results<span class=\"caret\"></span></a><ul class=\"dropdown-menu\"><li><a href=\"#\">Results</a></li></ul></li></ul></div></div></nav></template>"; });
+define('text!personalinfo.html', ['module'], function(module) { module.exports = "<template><h1>${message}</h1></template>"; });
 //# sourceMappingURL=app-bundle.js.map
