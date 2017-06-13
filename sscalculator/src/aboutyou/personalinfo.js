ea81a1900cf5ed9b904d93aebe621cacb67b320f
jmcacioppo <jcacioppo@pietech.com>
2017-06-13 12:43:33 -0400

    Removed retirement info page

--- sscalculator/src/aboutyou/personalinfo.js
+++ sscalculator/src/aboutyou/personalinfo.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
+import 'jquery-ui-dist';
+
 import {inject} from 'aurelia-framework';
 import {UserData} from '../services/userdata'
 
@@ -11,4 +14,19 @@ export class personalinfo {
     print() {
         console.log(this.userData);
     }
+
+    attached () {
+        $('#retire').slider({
+            range: true, 
+            min: 50, 
+            max: 100, 
+            values: [65, 91],
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "Retire at " + ui.values[ 0 ] + " - Live to " + ui.values[ 1 ] );
+            }
+        });
+
+        $( "#amount" ).val( "Retire at " + $( "#retire" ).slider( "values", 0 ) +
+            " - Live to  " + $( "#retire" ).slider( "values", 1 ) );
+   }
 }
