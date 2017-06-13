5cbf456242929ebde3ef97fff6da9ec0d46870e7
jmcacioppo <jcacioppo@PIE_MAIN.pietech.com>
2017-06-13 11:02:08 -0400

    Slider working kinda

--- sscalculator/src/aboutyou/retirementinfo.js
+++ sscalculator/src/aboutyou/retirementinfo.js
@@ -1,10 +1,26 @@
 import $ from 'jquery';
 import 'jquery-ui-dist';
+import {inject} from 'aurelia-framework';
+import {UserData} from '../services/userdata'
 
+@inject(UserData)
 export class retirementinfo {
+   constuctor(userData) {
+       this.userData = userData;
+   }
+   
    attached () {
-        //var slider = new Slider('#retire', {});
-        $('#retire').slider({});
+        $('#retire').slider({
+            range: true, 
+            min: 50, 
+            max: 100, 
+            values: [65, 91],
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            }
+        });
+        $( "#amount" ).val( "Retire at " + $( "#retire" ).slider( "values", 0 ) +
+            " - Live to  " + $( "#retire" ).slider( "values", 1 ) );
    }
 }
 
