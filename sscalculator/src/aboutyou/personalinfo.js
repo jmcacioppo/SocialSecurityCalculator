a9a96b5ae34966c453b001e2a8d9c3dfe09e4773
jmcacioppo <jcacioppo@pietech.com>
2017-06-15 09:29:33 -0400

    calculate works

--- sscalculator/src/aboutyou/personalinfo.js
+++ sscalculator/src/aboutyou/personalinfo.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'jquery-ui-dist';
 import * as bootstrapToggle from 'bootstrap-toggle';
 
+import 'src/services/constants.js'
 import {inject} from 'aurelia-framework';
 import {UserData} from '../services/userdata'
 
@@ -10,10 +11,9 @@ export class personalinfo {
     constructor(userData) {
         this.message = "Personal Information";
         this.userData = userData;
-        this.lol = "";
     }
 
-    calculate() { 
+    calculate() { //WORKS CORRECTLY
         var sal = this.userData.client.salary;
         var pia;
         var wage = [];
@@ -22,55 +22,49 @@ export class personalinfo {
         var topThirtyFive = [];
         var ssBase;
 
-        // console.log(this.userData);
-        // function put(ssBase) {
-        //     this.userData.client.ssBase = ssBase;
-        //     console.log(this.userData.client.ssBase);
-        // }
-
-        $.getJSON("src/services/constants.json", function(result){ //first year = 1956
-            if(sal > 0) { //Check if there is an inputted salary
-                projectedSal[result.wagePerc.length-1] = sal; //Current salary
-                for(var i = result.wagePerc.length - 2; i >= 0; i--) { //Loop through each wage percentage backwards so we go from current salary
-                    projectedSal[i] = projectedSal[i+1] - (projectedSal[i+1] * result.wagePerc[i+1]); //Calculate projected salary
-                    if(projectedSal[i] > result.allowedSalary[i]) { //Check allowed salary and calculate adjusted inflation accordingly
-                        inflationAdjusted[i] = result.allowedSalary[i] * result.inflationIndex[i];
-                    }
-                    else {
-                        inflationAdjusted[i] = projectedSal[i] * result.inflationIndex[i];
-                    }
+        //first year = 1956
+        if(sal > 0) { //Check if there is an inputted salary
+            projectedSal[wagePerc.length-1] = sal; //Current salary
+            for(var i = wagePerc.length - 2; i >= 0; i--) { //Loop through each wage percentage backwards so we go from current salary
+                projectedSal[i] = projectedSal[i+1] - (projectedSal[i+1] * wagePerc[i+1]); //Calculate projected salary
+                if(projectedSal[i] > allowedSalary[i]) { //Check allowed salary and calculate adjusted inflation accordingly
+                    inflationAdjusted[i] = allowedSalary[i] * inflationIndex[i];
                 }
-
-                inflationAdjusted = inflationAdjusted.sort((a, b) => a - b); //Sort adjusted inflation to get top 35
-                topThirtyFive = inflationAdjusted.slice(inflationAdjusted.length - 35, inflationAdjusted.length); 
-                pia = topThirtyFive.reduce((a, b) => a + b, 0) / 420; //Primary Insurance Amount
-
-                //Benefit Formula
-                var sum = parseInt(result.tier1) + parseInt(result.tier2); //Get sum of tier1 and tier2 constants
-                var tier1 = result.tier1 * result.tier1perc; //Tier1 for benefit formula
-                var tier2;
-                if(pia < (result.tier1 + result.tier2)) { //Tier2 for benefit formula
-                    tier2 = result.tier2 * result.tier2perc
-                } 
                 else {
-                    tier2 = (pia - sum) * result.tier2perc;
-                } 
-                var tier3 = (pia - sum) * result.tier3perc; //Tier3 for benefit formula
-
-                var sumOfTiers = tier1 + tier2 + tier3; //Add the tiers together
-                ssBase = sumOfTiers * 12; //This is the monthly base SS value
-                this.lol = ssBase;
-                console.log(this.lol);
-                //put(ssBase);
-                // this.userData.client.ssBase = ssBase;
-                // console.log(this.userData.client.ssBase);
-               
+                    inflationAdjusted[i] = projectedSal[i] * inflationIndex[i];
+                }
             }
-        });
 
-        // this.userData.client.ssBase = ssBase;
-        // console.log(this.userData.client.ssBase);
-        
+            inflationAdjusted = inflationAdjusted.sort((a, b) => a - b); //Sort adjusted inflation to get top 35
+            topThirtyFive = inflationAdjusted.slice(inflationAdjusted.length - 35, inflationAdjusted.length); 
+            pia = topThirtyFive.reduce((a, b) => a + b, 0) / 420; //Primary Insurance Amount
+
+            //Benefit Formula
+            var tier1, tier2, tier3;
+            var sum = consttier1 + consttier2; //Get sum of tier1 and tier2 constants
+            if(pia > consttier1) { //Tier1 for benefit formula
+                tier1 = consttier1 * tier1perc;
+            }
+            else tier1 = pia * tier1perc;
+             
+            
+            if(pia > sum) { //Tier2 for benefit formula
+                tier2 = consttier2 * tier2perc
+            } 
+            else tier2 = pia * tier2perc; 
+
+            if(pia > sum) {
+                tier3 = (pia - sum) * tier3perc; //Tier3 for benefit formula
+            }
+            else tier3 = 0;
+
+            var sumOfTiers = tier1 + tier2 + tier3; //Add the tiers together
+            ssBase = sumOfTiers * 12; //This is the monthly base SS value
+            this.userData.client.ssBase = ssBase;  
+        }
+        else {
+            alert('Salary must be greater than 0');
+        }
     }
 
     attached() {        
