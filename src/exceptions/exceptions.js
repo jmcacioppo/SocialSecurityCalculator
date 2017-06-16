ea1a946577e4b194f976c9cfcd27a7e0221b4c50
Sean McHugh <smchugh@PIE_MAIN.pietech.com>
2017-06-16 17:22:29 -0400

    almost got the exceptions page skeelton

--- src/exceptions/exceptions.js
+++ src/exceptions/exceptions.js
@@ -17,11 +17,15 @@ export class exceptions {
     constructor(userData, router)
     {
         this.clientMilitaryService = false;
+        this.clientBeganService = "";
+        this.clientEndService = "";
         this.clientWorkedOnAFarm = false;
         this.clientWorkedInAHousehold = false;
         this.clientWorkedOnARailroad = false;
         this.clientRecievePension = false;
         this.clientWorkedForeignGov = false;
+
+        this.clientCitizenship = true;
 
         this.spouseMilitaryService = false;
         this.spouseWorkedOnAFarm = false;
@@ -31,13 +35,65 @@ export class exceptions {
         this.spouseWorkedForeignGov = false;
 
         this.router = router;
+        this.userData = userData;
     }
 
     calculate()
     {
-        
+
 
         this.router.navigate('#/benefits');
+    }
+
+    attached()
+    {
+        if (this.userData.client.maritalStatus != "Married")
+            $('#spouse').hide();
+
+        $('#clientServed').hide();
+        $('#clientMilitaryService').change(function() {
+            console.log($(this).val());
+           $(this).is(':checked') ? $('#clientServed').show() : $('#clientServed').hide();
+        });
+
+        $('#clientMadeFarmMoney').hide();
+        $('#clientWorkedOnAFarm').change(function() {
+            console.log($(this).val());
+           $(this).is(':checked') ? $('#clientMadeFarmMoney').show() : $('#clientMadeFarmMoney').hide();
+        });
+
+        $('#clientMadeHouseHoldMoney').hide();
+        $('#clientWorkedInAHousehold').change(function() {
+            console.log($(this).val());
+           $(this).is(':checked') ? $('#clientMadeHouseHoldMoney').show() : $('#clientMadeHouseHoldMoney').hide();
+        });
+
+        $('#clientRailroadBox').hide();
+        $('#clientWorkedOnARailroad').change(function() {
+            console.log($(this).val());
+           $(this).is(':checked') ? $('#clientRailroadBox').show() : $('#clientRailroadBox').hide();
+        });
+
+        $('#clientPensionBox').hide();
+        $('#clientRecievePension').change(function() {
+            console.log($(this).val());
+           $(this).is(':checked') ?  $('#clientPensionBox').show() : $('#clientPensionBox').hide();
+        });
+
+        $('#clientCitizenship').hide();
+        $('#clientWorkedForeignGov').change(function() {
+            switch ($ (this).val() )
+            {
+                
+            }
+        });
+
+        $('#clientWorkInstrumentality').hide();
+        $('#clientInstrumentality').change(function() {
+            console.log($(this).val());
+           
+        });
+
     }
     
 }
