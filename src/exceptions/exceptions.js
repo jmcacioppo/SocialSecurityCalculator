c2b6402afb89e2ce527ed0141d315ad5861b3412
Sean McHugh <smchugh@PIE_MAIN.pietech.com>
2017-06-16 12:31:58 -0400

    got a skeleton of the exceptions page

--- src/exceptions/exceptions.js
+++ src/exceptions/exceptions.js
@@ -1,3 +1,43 @@
+import $ from 'jquery';
+import 'jquery-ui-dist';
+import * as bootstrapToggle from 'bootstrap-toggle';
+import * as ionRangeSlider from "ion-rangeslider";
+import moment from 'moment';
+
+import 'src/services/constants.js';
+import {inject} from 'aurelia-framework';
+import {UserData} from '../services/userdata';
+import {Router} from 'aurelia-router';
+import {wagePerc, allowedSalary, inflationIndex, tier1perc, tier2perc, tier3perc, 
+    tier1, consttier1, consttier2,subEarningsPerc, EL1943plus, EL1955, EL1956, EL1957,
+    EL1958, EL1959, EL1960plus} from 'src/services/constants.js';
+
+@inject(UserData, Router)
 export class exceptions {
+    constructor(userData, router)
+    {
+        this.clientMilitaryService = false;
+        this.clientWorkedOnAFarm = false;
+        this.clientWorkedInAHousehold = false;
+        this.clientWorkedOnARailroad = false;
+        this.clientRecievePension = false;
+        this.clientWorkedForeignGov = false;
+
+        this.spouseMilitaryService = false;
+        this.spouseWorkedOnAFarm = false;
+        this.spouseWorkedInAHousehold = false;
+        this.spouseWorkedOnARailroad = false;
+        this.spouseRecievePension = false;
+        this.spouseWorkedForeignGov = false;
+
+        this.router = router;
+    }
+
+    calculate()
+    {
+        
+
+        this.router.navigate('#/benefits');
+    }
     
 }
